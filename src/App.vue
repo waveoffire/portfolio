16b2620e926e6c12a43d5456812033eca871af77
piotr.smilgin <piotrsmilgin7@gmail.com>
2019-01-27 17:02:14 +0100

    scrolling to items and myphoto

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,41 @@
 <template>
   <div class="over" id="app">
     <div class="row menu">
-      <span class="myname col-8">Piotr Smilgin</span>
-      <span class="menuspan col-1">O mnie</span>
-      <span class="menuspan col-1">Umiejętności</span>
-      <span class="menuspan col-1">Moje projekty</span>
-      <span class="menuspan col-1">Kontakt</span>
+      <span class="myname col-8" @click="scrolling('header')">Piotr Smilgin</span>
+      <span class="menuspan col-1" @click="scrolling('about')">O mnie</span>
+      <span class="menuspan col-1" @click="scrolling('experience')">Umiejętności</span>
+      <span class="menuspan col-1" @click="scrolling('projects')">Moje projekty</span>
+      <span class="menuspan col-1" @click="scrolling('contact')">Kontakt</span>
     </div>
-    <div class="header">
+    <div id="headerstart" class="header">
       <div class="header-box">
         <div>Junior Front-end Developer</div>
         <div class="headername">Piotr Smilgin</div>
         <div class="links">
-          <i class="fa fa-github" aria-hidden="true"></i>
-          <i class="fa fa-facebook-official" aria-hidden="true"></i>
+          <a href="https://github.com/waveoffire" target="_blank">
+            <i class="fa fa-github" aria-hidden="true"></i>
+          </a>
+          <a href="https://www.facebook.com/piotr.smilgin.7" target="_blank">
+            <i class="fa fa-facebook-official" aria-hidden="true"></i>
+          </a>
         </div>
       </div>
     </div>
-    <div class="container">
+    <div id="aboutstart" class="container">
       <h1>O mnie</h1>
-      <div
-        id="about"
-        class="row about"
-      >Cześć, nazywam się Piotr Smilgin, a to moje portfolio. Mieszkam w Poznaniu. Pracuje jako serwisant komputerowy oraz dodatkowo, hobistycznie programuje.
-        <br>Uwielbiam programować, zwłaszcza front-end. Od dłuższego czasu programuje w Vue.js. Chciałbym w przyszłości zostać programistą Front-end.
-        <br>Zapraszam do obejrzenia moich projektów. Jeśli masz dla mnie propozycje współpracy - zapraszam do kontaktu.
+      <div style="padding-top:100px;" class="row">
+        <div
+          class="col-7 about"
+        >Cześć, nazywam się Piotr Smilgin, a to moje portfolio. Mieszkam w Poznaniu. Pracuje jako serwisant komputerowy oraz dodatkowo, hobistycznie programuje.
+          <br>Uwielbiam programować, zwłaszcza front-end. Od dłuższego czasu programuje w Vue.js. Chciałbym w przyszłości zostać programistą Front-end.
+          <br>Zapraszam do obejrzenia moich projektów. Jeśli masz dla mnie propozycje współpracy - zapraszam do kontaktu.
+        </div>
+        <div class="myphoto col-5">
+          <img src="./assets/photo/myphoto.jpg">
+        </div>
       </div>
     </div>
-    <div class="experience container">
+    <div id="experiencestart" class="experience container">
       <h1>Moje umiejętności</h1>
       <div class="expDiv">
         <img class="expPhoto" src="./assets/photo/html.png" alt="HTML">
@@ -77,7 +85,35 @@
       </div>
       <span class="expSpan">NPM i Yarn używam naprzemiennie</span>
     </div>
-    <div id="contact" class="contact">XD</div>
+    <div id="projectsstart" class="container">
+      <h1>Moje projekty</h1>
+      <div style="padding-top:100px;" class="row">
+        <div class="col-6 projectdiv">
+          <img class="projectphoto" src="./assets/photo/homepage.png">
+          <div class="projecthover">
+            <span>
+              Przykładowy projekt strony głownej w vanilla javascript i css
+              <br>
+              <br>
+              <button class="btn btn-outline-light">GITHUB</button>
+              <button class="btn btn-outline-light">LIVE</button>
+            </span>
+          </div>
+        </div>
+        <div class="col-6 projectdiv">
+          <img class="projectphoto" src="./assets/photo/zdzis.png">
+          <div class="projecthover">
+            <span>Projekt w trakcie realizacji z użyciem Vue.js, pokazujący ciekawe oferty oraz zmiany w cenach samochodów
+              <br>
+              <br>
+
+              <button class="btn btn-outline-light">LIVE</button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="contactstart" class="contact"></div>
   </div>
 </template>
 
@@ -86,6 +122,14 @@ export default {
   name: "app",
   data() {
     return {};
+  },
+  methods: {
+    scrolling(nazwa) {
+      document.querySelector("#" + nazwa + "start").scrollIntoView({
+        behavior: "smooth",
+        alignTo: "False"
+      });
+    }
   }
 };
 </script>
@@ -146,8 +190,23 @@ body {
 h1 {
   padding: 30px;
   text-align: center;
+  padding-top: 150px;
 }
 .about {
+  display: inline-block;
+  width: 60%;
+  font-size: 1.4em;
+  padding-top: 90px;
+  height: 80vh;
+}
+.myphoto {
+  display: inline-block;
+  width: 40%;
+}
+.myphoto img {
+  width: 100%;
+  border-radius: 50%;
+  transform: translateX(10%);
 }
 .contact {
   -webkit-clip-path: polygon(0 13vh, 100% 0, 100% 50vh, 0 50vh);
@@ -156,6 +215,7 @@ h1 {
   background-image: linear-gradient(to bottom, #002d47, #001927);
   background-repeat: no-repeat;
   background-size: 100%;
+
   height: 50vh;
   color: white;
   text-align: center;
@@ -163,6 +223,10 @@ h1 {
 }
 .experience {
   text-align: center;
+}
+.experience h1 {
+  padding-bottom: 100px;
+  padding-top: 20px;
 }
 .expPhoto {
   height: 150px;
@@ -182,5 +246,46 @@ h1 {
 .links i {
   margin: 10px;
   cursor: pointer;
+}
+a {
+  color: rgba(255, 255, 255, 0.8);
+}
+a:hover {
+  color: rgba(255, 255, 255, 1);
+}
+.projectdiv {
+  width: 50%;
+  position: relative;
+  padding: 0 10px 0 10px;
+}
+.projectphoto {
+  width: 100%;
+  border-radius: 10px;
+}
+.projecthover {
+  opacity: 0;
+  margin: 0 10px 0 10px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  color: white;
+  background-image: linear-gradient(
+    to right bottom,
+    rgba(0, 4, 255, 0.4),
+    rgba(4, 235, 93, 0.6)
+  );
+  text-align: center;
+  border-radius: 10px;
+}
+.projecthover span {
+  top: 30%;
+  width: 100%;
+  padding: 15px;
+  position: relative;
+}
+.projecthover:hover {
+  opacity: 1;
 }
 </style>
