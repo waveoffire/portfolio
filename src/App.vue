fe2929224c8f05d91785d92bf8c0883095ce2594
piotr.smilgin <piotrsmilgin7@gmail.com>
2019-01-27 13:33:20 +0100

    add font-awesome and my experience

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,73 @@
       <div class="header-box">
         <div>Junior Front-end Developer</div>
         <div class="headername">Piotr Smilgin</div>
+        <div class="links">
+          <i class="fa fa-github" aria-hidden="true"></i>
+          <i class="fa fa-facebook-official" aria-hidden="true"></i>
+        </div>
       </div>
     </div>
-    <div
-      id="about"
-      class="row about"
-    >Cześć, nazywam się Piotr Smilgin, a to moje portfolio. Mieszkam w Poznaniu. Pracuje jako serwisant komputerowy oraz dodatkowo, hobistycznie programuje.
-      <br>Uwielbiam programować, zwłaszcza front-end. Od dłuższego czasu programuje w Vue.js.Chciałbym w przyszłości zostać programistą Front-end.
-      <br>Zapraszam do obejrzenia moich projektów. Jeśli masz dla mnie propozycje współpracy - zapraszam do kontaktu.
+    <div class="container">
+      <h1>O mnie</h1>
+      <div
+        id="about"
+        class="row about"
+      >Cześć, nazywam się Piotr Smilgin, a to moje portfolio. Mieszkam w Poznaniu. Pracuje jako serwisant komputerowy oraz dodatkowo, hobistycznie programuje.
+        <br>Uwielbiam programować, zwłaszcza front-end. Od dłuższego czasu programuje w Vue.js. Chciałbym w przyszłości zostać programistą Front-end.
+        <br>Zapraszam do obejrzenia moich projektów. Jeśli masz dla mnie propozycje współpracy - zapraszam do kontaktu.
+      </div>
     </div>
+    <div class="experience container">
+      <h1>Moje umiejętności</h1>
+      <div class="expDiv">
+        <img class="expPhoto" src="./assets/photo/html.png" alt="HTML">
+
+        <p class="expSpan">HTML to podstawa każdej mojej strony</p>
+      </div>
+      <div class="expDiv">
+        <img class="expPhoto" src="./assets/photo/css.png" alt="CSS">
+        <p class="expSpan">Animacje w CSS nie są mi obce</p>
+      </div>
+      <div class="expDiv">
+        <img class="expPhoto" src="./assets/photo/JavaScript.png" alt="JAVASCRIPT">
+        <span class="expSpan">API i Front pisze w JavaScript</span>
+      </div>
+      <div class="expDiv">
+        <img class="expPhoto" src="./assets/photo/vue.png" alt="VUE.JS">
+        <span class="expSpan">Vue.js to framework który skradł mi serce</span>
+      </div>
+      <div class="expDiv">
+        <img class="expPhoto" src="./assets/photo/bootstrap.png" alt="BOOTSTRAP">
+        <span class="expSpan">Bootstrap bez niego życie nie było by takie proste</span>
+      </div>
+      <div class="expDiv">
+        <img class="expPhoto" src="./assets/photo/json.png" alt="JSON">
+        <span class="expSpan">Dane pomiedzy api-front przesyłam jako JSON</span>
+      </div>
+      <div class="expDiv">
+        <img class="expPhoto" src="./assets/photo/Git.png" alt="GIT">
+        <span class="expSpan">GIT to System kontroli wersji który ratuje życie</span>
+      </div>
+      <div class="expDiv">
+        <img class="expPhoto" src="./assets/photo/typescript.png" alt="TYPESCRIPT">
+        <span class="expSpan">Do swoich projektów używam również TypeScript</span>
+      </div>
+      <div class="expDiv">
+        <img class="expPhoto" src="./assets/photo/node.png" alt="NODE">
+        <span class="expSpan">API bez Node.js by nie istniało</span>
+      </div>
+      <div>
+        <div class="expDiv npm">
+          <img class="expPhoto" src="./assets/photo/npm.svg" alt="NPM">
+        </div>
+        <div class="expDiv npm">
+          <img class="expPhoto" src="./assets/photo/yarn.png" alt="YARN">
+        </div>
+        <!--  <img class="expPhoto" src="./assets/photo/mysql.png" alt="MYSQL"> -->
+      </div>
+      <span class="expSpan">NPM i Yarn używam naprzemiennie</span>
+    </div>
+    <div id="contact" class="contact">XD</div>
   </div>
 </template>
 
@@ -43,6 +101,7 @@ export default {
   right: 0;
   z-index: 100;
   color: rgb(231, 0, 0);
+  border-bottom: 1px solid rgba(128, 128, 128, 0.473);
 }
 body {
   font-family: "Roboto", sans-serif;
@@ -84,7 +143,44 @@ body {
   font-size: 2.2em;
   line-height: 0.7em;
 }
+h1 {
+  padding: 30px;
+  text-align: center;
+}
 .about {
-  padding: 10%;
+}
+.contact {
+  -webkit-clip-path: polygon(0 13vh, 100% 0, 100% 50vh, 0 50vh);
+  clip-path: polygon(0 13vh, 100% 0, 100% 50vh, 0 50vh);
+  z-index: 50;
+  background-image: linear-gradient(to bottom, #002d47, #001927);
+  background-repeat: no-repeat;
+  background-size: 100%;
+  height: 50vh;
+  color: white;
+  text-align: center;
+  font-size: 2.8em;
+}
+.experience {
+  text-align: center;
+}
+.expPhoto {
+  height: 150px;
+}
+.expDiv {
+  display: inline-block;
+  width: 200px;
+  text-align: center;
+  margin: 5px;
+}
+.expSpan {
+  display: block;
+}
+.npm {
+  width: 400px;
+}
+.links i {
+  margin: 10px;
+  cursor: pointer;
 }
 </style>
