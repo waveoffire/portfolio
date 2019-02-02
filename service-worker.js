1f64752e51c0e27f7afbdbbb0519b717e1817025
piotr.smilgin <piotrsmilgin7@gmail.com>
2019-02-02 16:27:35 +0100

    dist

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2ab68e8a9406ee1c206a6e0a946ee74a.js"
+  "/portfolio/precache-manifest.e7928d31b7fd9346e2208d762b68e381.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "portfolio"});
