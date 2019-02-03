6bec6d52a15a04593b97cabac177f9b8de6224d3
piotr.smilgin <piotrsmilgin7@gmail.com>
2019-02-03 22:54:43 +0100

    rebuild

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.94b2bf8279ecefd6e98bb2e94a90d680.js"
+  "/portfolio/precache-manifest.31801c8dfa103df0096f8bcb6ff96bfb.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "portfolio"});
