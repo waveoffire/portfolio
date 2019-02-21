4534be32269e2469aa73316f2f31fa02ff38f766
piotr.smilgin <piotrsmilgin7@gmail.com>
2019-02-21 21:26:10 +0100

    dist

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6f70cdf7e0b126874735e0f488e37ee8.js"
+  "/portfolio/precache-manifest.d181205722410d603fb20e0bf63c18a3.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "portfolio"});
